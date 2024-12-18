95666529626e7ebc4f0de00c4c95afd6f60e3169
Arpandobariya15 <work.arpan8980@gmail.com>
2024-12-18 22:57:09 +0530

    route-1

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,12 @@ require('./Models/db');
 const PORT = process.env.PORT || 8080;
 const TaskRouter = require('./Routes/TaskRouter');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 
 app.get('/', (req, res) => {
     res.send('Hello from the server');
 });
-app.use(cors())
+// app.use(cors())
 app.use(bodyParser.json());
 app.use('/tasks', TaskRouter)
 
